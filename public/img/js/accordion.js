0765eacc91215697c7acda8a8bfad1cf536ac35b
lxzanaa <zari.weone2010@gmail.com>
2024-10-09 19:45:10 +0300

    add

---
+++ public/img/js/accordion.js
@@ -0,0 +1,45 @@
+const accordionButtons = document.querySelectorAll('.accordion-button')
+
+accordionButtons.forEach(function (accordionBtn) {
+  accordionBtn.addEventListener('click', function () {
+    const isOpen = accordionBtn.classList.toggle('max-h-[100%]')
+
+    const texts = accordionBtn.querySelectorAll('.faq-text')
+    const images = accordionBtn.querySelectorAll('.faq-img')
+    const icon = accordionBtn.querySelector('.accordion-icon')
+
+    texts.forEach(function (text) {
+      if (isOpen) {
+        text.classList.remove('hidden', 'opacity-0', 'max-h-0')
+        text.classList.add('opacity-1', 'max-h-[100%]')
+      } else {
+        text.classList.add('hidden', 'opacity-0', 'max-h-0')
+        text.classList.remove('opacity-1', 'max-h-[100%]')
+      }
+    })
+
+    images.forEach(function (image) {
+      if (isOpen) {
+        image.classList.remove('opacity-0', 'max-h-0')
+        image.classList.add('opacity-1', 'max-h-[100%]')
+      } else {
+        image.classList.add('opacity-0', 'max-h-0')
+        image.classList.remove('opacity-1', 'max-h-[100%]')
+      }
+    })
+
+    icon.src = isOpen ? './img/accordion/minus.svg' : './img/accordion/plus.svg'
+  })
+})
+
+let mobileAccordionBtns = document.querySelectorAll('#mobile-accordion-btn')
+
+mobileAccordionBtns.forEach(function (faqBtn, index) {
+  faqBtn.addEventListener('click', function () {
+    let mobileAccordionInfo = document.querySelectorAll(
+      '#mobile-accordion-info'
+    )[index]
+    mobileAccordionInfo.classList.toggle('max-h-0')
+    mobileAccordionInfo.classList.toggle('max-h-full')
+  })
+})
